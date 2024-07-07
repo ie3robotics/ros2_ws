db86731d360c45b2502959d761e7a32ce287cc71
root <root@Ros2.myguest.virtualbox.org>
2024-07-07 17:42:24 -0500

    added service client call into turtlesim controller example

--- src/my_robot_controller/my_robot_controller/turtle_controller.py
+++ src/my_robot_controller/my_robot_controller/turtle_controller.py
@@ -3,12 +3,15 @@ import rclpy
 from rclpy.node import Node
 from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
+from turtlesim.srv import SetPen
+from functools import partial
 
 
 class TurtleControllerNode(Node):
 
     def __init__(self):
         super().__init__("turtle_controller")
+        self.previous_x_ = 0
         self.cmd_vel_publisher = self.create_publisher(Twist, "/turtle1/cmd_vel", 10)
         self.pose_subscriber = self.create_subscription(Pose, "/turtle1/pose", self.pose_callback, 10)
         self.get_logger().info("Turtle Controller started")
@@ -23,6 +26,40 @@ class TurtleControllerNode(Node):
             cmd.angular.z = 0.0
 
         self.cmd_vel_publisher.publish(cmd)
+
+        if pose.x > 5.5 and self.previous_x_ <= 5.5:
+            self.previous_x_ = pose.x
+            self.get_logger().info("set color to red")
+            self.call_set_pen_service(225, 0, 0, 3, 0)
+
+        elif pose.x <= 5.5 and self.previous_x_ > 5.5:
+            self.previous_x_ = pose.x
+            self.get_logger().info("set color to green")
+            self.call_set_pen_service(0, 225, 0, 3, 0)
+
+    def call_set_pen_service(self, r, g, b, width, off):
+        client = self.create_client(SetPen, "/turtle1/set_pen")
+        while not client.wait_for_service(1.0):
+            self.get_logger().warn("waiting for service...")
+
+        request = SetPen.Request()
+        request.r = r
+        request.g = g
+        request.b = b
+        request.width = width
+        request.off = off
+
+        future = client.call_async(request)
+        future.add_done_callback(partial(self.callback_set_pen))
+
+
+    def callback_set_pen(self, future):
+        try:
+            response = future.result()
+        except Exception as e:
+            self.get_logger().error("Service call failed: %r" % (e,))
+
+
 
 
 
