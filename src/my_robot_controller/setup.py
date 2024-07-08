ff2c114e490d248a8cd28ddb927d6f5d68afee56
Nathan <nathancharles4321@gmail.com>
2024-07-08 15:33:58 -0500

    made new node face_tracker_node that controlls the orintation of the robots camera and displays it via openCV

--- src/my_robot_controller/setup.py
+++ src/my_robot_controller/setup.py
@@ -23,8 +23,9 @@ setup(
             "test_node = my_robot_controller.my_first_node:main",
             "draw_circle = my_robot_controller.draw_circle:main",
             "pose_subscriber = my_robot_controller.pose_subscriber:main",
-            "turtle_controller = my_robot_controller.turtle_controller:main"
-            "transbot_keyboard = my_robot_controller.transbot_keyboard:main"
+            "turtle_controller = my_robot_controller.turtle_controller:main",
+            "face_tracker_node = my_robot_controller.face_tracker_node:main"
         ],
+        
     },
 )
